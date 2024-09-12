8494ee51a0ca8dae07ba2279c88fd112e7921750
ilias195 <iliasafc@outlook.com>
2024-09-12 12:20:07 +0200

    complete opdracht 1

---
+++ op1.js
@@ -0,0 +1,10 @@
+
+const date1 = new Date (1985,7,12);
+const date2= new Date (1955,9,23);
+const date3= new Date (2015,8,31);
+const date4= new Date (1885,1,4);
+
+console.log (date1);
+console.log (date2);
+console.log (date3);
+console.log (date4);
